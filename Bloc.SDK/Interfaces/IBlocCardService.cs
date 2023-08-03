aba81c80574423b86bff0e1ce191b83fd041f299
LontraX <olumidejoda@gmail.com>
2023-08-03 16:52:01 +0100

    increment

---
+++ Bloc.SDK/Interfaces/IBlocCardService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloc.SDK.Interfaces
+{
+    public interface IBlocCardService
+    {
+        Task GetCards();
+        Task GetCardsById();
+        Task FundCard();
+        Task BlockCard();
+        Task FreezeCard();
+    }
+}
